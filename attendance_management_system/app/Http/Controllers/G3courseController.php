16836a4e8ac4176f5ee0daaa737eb254cab8fb45
jananikangesan <janakanges1996@gmail.com>
2021-01-08 13:39:40 +0530

    3g students attendance controller created

--- attendance_management_system/app/Http/Controllers/G3courseController.php
+++ attendance_management_system/app/Http/Controllers/G3courseController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Course;
-//use App\Models\Attendance_3G_Student;
+use App\Models\Attendance_3G_Student;
 use App\Student;
 use Illuminate\Http\Request;
 
@@ -14,5 +14,17 @@ class G3courseController extends Controller
         return view('level_3.3gcourse.3gcourses', compact('g3_courses'));
 
         //dd('$s3_courses');
+
+
+
     }
+
+    public function attendance(Request $request)
+    {
+        $course = $request->input('g3_course');
+        $attendances = Attendance_3G_Student::with('student')->where('course_code','=', $course)->paginate(25);
+        $g3_courses = Course::where('course_level', '3G')->where('semester', '2')->select('course_code')->get();
+        return view('level_3.3gcourse.3g_course_attendance', compact('course', 'attendances', 'g3_courses'));
+    }
+
 }
