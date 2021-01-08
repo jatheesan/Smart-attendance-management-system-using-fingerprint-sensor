0b25aaae68fedb99954fe445feddceb0925789ff
jananikangesan <janakanges1996@gmail.com>
2021-01-08 21:04:46 +0530

    3m course controller is created

--- attendance_management_system/app/Http/Controllers/M3courseController.php
+++ attendance_management_system/app/Http/Controllers/M3courseController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Course;
-//use App\Models\Attendance_3M_Student;
+use App\Models\Attendance_3M_Student;
 use App\Student;
 use Illuminate\Http\Request;
 
@@ -14,5 +14,13 @@ class M3courseController extends Controller
         return view('level_3.3mcourse.3mcourses', compact('m3_courses'));
 
         //dd('$m3_courses');
+    }
+
+    public function attendance(Request $request)
+    {
+        $course = $request->input('m3_course');
+        $attendances = Attendance_3M_Student::with('student')->where('course_code','=', $course)->paginate(25);
+        $m3_courses = Course::where('course_level', '3M')->where('semester', '2')->select('course_code')->get();
+        return view('level_3.3mcourse.3m_course_attendance', compact('course', 'attendances', 'm3_courses'));
     }
 }
