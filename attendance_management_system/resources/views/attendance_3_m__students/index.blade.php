81702eac7ae8188d0c6d9cbcf4ef8916f380fd6a
jananikangesan <janakanges1996@gmail.com>
2021-01-08 21:01:59 +0530

    3m student attendance index page is created

--- attendance_management_system/resources/views/attendance_3_m__students/index.blade.php
+++ attendance_management_system/resources/views/attendance_3_m__students/index.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
-
-@section('content')
+@extends('level_3.3mcourse.3mcourses')
+@section('pagetitle', 'Attandance/level3/3M')
+@section('levelcontent')
 
     @if(Session::has('success_message'))
         <div class="alert alert-success">
@@ -18,13 +18,9 @@
 
         <div class="panel-heading clearfix">
 
-            <div class="pull-left">
-                <h4 class="mt-5 mb-5">Attendance 3 M  Students</h4>
-            </div>
-
             <div class="btn-group btn-group-sm pull-right" role="group">
                 <a href="{{ route('attendance_3_m__students.attendance_3_m__student.create') }}" class="btn btn-success" title="Create New Attendance 3 M  Student">
-                    <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                    <span class="fa fa-plus" aria-hidden="true"></span>
                 </a>
             </div>
 
@@ -32,13 +28,13 @@
         
         @if(count($attendance3MStudents) == 0)
             <div class="panel-body text-center">
-                <h4>No Attendance 3 M  Students Available.</h4>
+                <h4>No Attendance 3M Available.</h4>
             </div>
         @else
         <div class="panel-body panel-body-with-table">
             <div class="table-responsive">
 
-                <table class="table table-striped ">
+                <table class="table table-striped table-hover table-bordered">
                     <thead>
                         <tr>
                             <th>Course Code</th>
@@ -46,8 +42,7 @@
                             <th>Hours</th>
                             <th>Hall</th>
                             <th>Attendance Mark</th>
-
-                            <th></th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -57,7 +52,17 @@
                             <td>{{ $attendance3MStudent->date }}</td>
                             <td>{{ $attendance3MStudent->hours }}</td>
                             <td>{{ $attendance3MStudent->hall }}</td>
-                            <td>{{ optional($attendance3MStudent->student)->st_regno }}</td>
+                            
+                            <td>
+                                @if (is_array($attendance3MStudent->attendance_mark) || is_object($attendance3MStudent->attendance_mark))
+                                
+                                    @foreach($attendance3MStudent->attendance_mark as $student)
+                                        {{$student}},
+                                    @endforeach
+                                @else
+                                {{ __('zero attendance') }}
+                                @endif  
+                            </td>
 
                             <td>
 
@@ -67,14 +72,14 @@
 
                                     <div class="btn-group btn-group-xs pull-right" role="group">
                                         <a href="{{ route('attendance_3_m__students.attendance_3_m__student.show', $attendance3MStudent->id ) }}" class="btn btn-info" title="Show Attendance 3 M  Student">
-                                            <span class="glyphicon glyphicon-open" aria-hidden="true"></span>
+                                            <i class="fa fa-caret-square-o-up" aria-hidden="true"></i>
                                         </a>
                                         <a href="{{ route('attendance_3_m__students.attendance_3_m__student.edit', $attendance3MStudent->id ) }}" class="btn btn-primary" title="Edit Attendance 3 M  Student">
-                                            <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                                            <i class="fa fa-pencil" aria-hidden="true"></i>
                                         </a>
 
                                         <button type="submit" class="btn btn-danger" title="Delete Attendance 3 M  Student" onclick="return confirm(&quot;Click Ok to delete Attendance 3 M  Student.&quot;)">
-                                            <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                                            <i class="fa fa-trash" aria-hidden="true"></i>
                                         </button>
                                     </div>
 
