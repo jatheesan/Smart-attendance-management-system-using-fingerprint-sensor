4dda405c1d499ac0902d838952f9628f570cc53e
jananikangesan <janakanges1996@gmail.com>
2021-01-09 18:06:35 +0530

    3m course controller modified

--- attendance_management_system/app/Http/Controllers/M3courseController.php
+++ attendance_management_system/app/Http/Controllers/M3courseController.php
@@ -16,11 +16,22 @@ class M3courseController extends Controller
         //dd('$m3_courses');
     }
 
+    // public function attendance(Request $request)
+    // {
+    //     $course = $request->input('m3_course');
+    //     $attendances = Attendance_3M_Student::with('student')->where('course_code','=', $course)->paginate(25);
+    //     $m3_courses = Course::where('course_level', '3M')->where('semester', '2')->select('course_code')->get();
+    //     return view('level_3.3mcourse.3m_course_attendance', compact('course', 'attendances', 'm3_courses'));
+    // }
+
+    // function for printing attendance like school register
+
     public function attendance(Request $request)
     {
         $course = $request->input('m3_course');
         $attendances = Attendance_3M_Student::with('student')->where('course_code','=', $course)->paginate(25);
         $m3_courses = Course::where('course_level', '3M')->where('semester', '2')->select('course_code')->get();
-        return view('level_3.3mcourse.3m_course_attendance', compact('course', 'attendances', 'm3_courses'));
+        $m3_st=Student::where('st_level','3M')->get();;
+        return view('level_3.3mcourse.3m', compact('course', 'attendances', 'm3_courses','m3_st'));
     }
 }
