d1f5b3ebaa4342a4f1036f7afacc9193fb2624cc
jananikangesan <janakanges1996@gmail.com>
2021-01-09 18:07:34 +0530

    3s course controller modified

--- attendance_management_system/app/Http/Controllers/S3courseController.php
+++ attendance_management_system/app/Http/Controllers/S3courseController.php
@@ -16,25 +16,25 @@ class S3courseController extends Controller
         //dd('$s3_courses');
     }
 
+    // public function attendance(Request $request)
+    // {
+    //     $course = $request->input('s3_course');
+    //     $attendances = Attendance_3S_Student::with('student')->where('course_code','=', $course)->paginate(25);
+    //     $s3_courses = Course::where('course_level', '3S')->where('semester', '2')->select('course_code')->get();
+    //     return view('level_3.3scourse.3s_course_attendance', compact('course', 'attendances', 's3_courses'));
+       
+    // }
+
+   // function for printing attendance like school register
+
     public function attendance(Request $request)
     {
         $course = $request->input('s3_course');
         $attendances = Attendance_3S_Student::with('student')->where('course_code','=', $course)->paginate(25);
         $s3_courses = Course::where('course_level', '3S')->where('semester', '2')->select('course_code')->get();
-        return view('level_3.3scourse.3s_course_attendance', compact('course', 'attendances', 's3_courses'));
-       
+        $s3_st=Student::where('st_level','3S')->get();;
+        return view('level_3.3scourse.3s', compact('course', 'attendances', 's3_courses','s3_st'));
     }
-
-//    // function for printing attendance like school register
-
-//     public function attendance(Request $request)
-//     {
-//         $course = $request->input('s3_course');
-//         $attendances = Attendance_3S_Student::with('student')->where('course_code','=', $course)->paginate(25);
-//         $s3_courses = Course::where('course_level', '3S')->where('semester', '2')->select('course_code')->get();
-//         $s3_st=Student::where('st_level','3S')->get();;
-//         return view('level_3.3scourse.3s', compact('course', 'attendances', 's3_courses','s3_st'));
-//     }
 
 
 }
