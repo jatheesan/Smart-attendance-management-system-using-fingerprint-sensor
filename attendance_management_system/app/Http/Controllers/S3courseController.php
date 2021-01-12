65d5325d77352b4bf1d26d4a81cc5a8aaccdaec8
jananikangesan <janakanges1996@gmail.com>
2021-01-12 12:22:06 +0530

    3s course controller modified

--- attendance_management_system/app/Http/Controllers/S3courseController.php
+++ attendance_management_system/app/Http/Controllers/S3courseController.php
@@ -32,8 +32,11 @@ class S3courseController extends Controller
         $course = $request->input('s3_course');
         $attendances = Attendance_3S_Student::with('student')->where('course_code','=', $course)->paginate(25);
         $s3_courses = Course::where('course_level', '3S')->where('semester', '2')->select('course_code')->get();
-        $s3_st=Student::where('st_level','3S')->get();;
-        return view('level_3.3scourse.3s', compact('course', 'attendances', 's3_courses','s3_st'));
+        $s3_st=Student::where('st_level','3S')->get();
+        $count3s = Student::where('st_level', '3S')->count();
+        $s3_cname = Course::where('course_level', '3S')->get();
+        $s3_coursecount = Attendance_3S_Student::where('course_code',$course )->count('date');
+        return view('level_3.3scourse.3s', compact('course', 'attendances', 's3_courses','s3_st','count3s','s3_coursecount','s3_cname'));
     }
 
 
