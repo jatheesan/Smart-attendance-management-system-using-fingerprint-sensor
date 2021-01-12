188faa2dc921bf68d979d104e35d1b90b2691b77
jananikangesan <janakanges1996@gmail.com>
2021-01-12 12:23:22 +0530

    3g course controller modified

--- attendance_management_system/app/Http/Controllers/G3courseController.php
+++ attendance_management_system/app/Http/Controllers/G3courseController.php
@@ -34,8 +34,12 @@ class G3courseController extends Controller
         $course = $request->input('g3_course');
         $attendances = Attendance_3G_Student::with('student')->where('course_code','=', $course)->paginate(25);
         $g3_courses = Course::where('course_level', '3G')->where('semester', '2')->select('course_code')->get();
-        $g3_st=Student::where('st_level','3G')->get();;
-        return view('level_3.3gcourse.3g', compact('course', 'attendances', 'g3_courses','g3_st'));
+        $g3_st=Student::where('st_level','3G')->get();
+        $count3g = Student::where('st_level', '3G')->count();
+        $g3_cname = Course::where('course_level', '3G')->get();
+        $g3_coursecount = Attendance_3G_Student::where('course_code',$course )->count('date');
+
+        return view('level_3.3gcourse.3g', compact('course', 'attendances', 'g3_courses','g3_st','count3g','g3_coursecount','g3_cname'));
     }
 
 }
